9003a8e8b0e399e86ef68aaf70d077c2c4cfb9c1
Mariana <162517839+marianarosalita@users.noreply.github.com>
2025-07-06 00:22:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1200,
     loop: true, 
 });
+
+tsParticles.load("tsparticles", {
+  fullScreen: { enable: false },
+  background: {
+    color: {
+      value: "transparent"
+    }
+  },
+  particles: {
+    number: {
+      value: 40,
+      density: {
+        enable: true,
+        area: 800
+      }
+    },
+    shape: {
+      type: "star"
+    },
+    size: {
+      value: 4,
+      random: true
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      outModes: {
+        default: "bounce"
+      }
+    },
+    opacity: {
+      value: 0.6,
+      random: true
+    },
+    color: {
+      value: "#000000"
+    }
+  }
+});
